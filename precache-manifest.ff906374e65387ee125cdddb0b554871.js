ea1fcf48377a89e94079bc66bcda23e9b2b7b930
Fatih <kresnofatihimani.private@gmail.com>
2020-09-23 13:47:33 +0700

    Updates

---
+++ precache-manifest.ff906374e65387ee125cdddb0b554871.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8a025d572ee8ef23e58e8fa9e655a0",
+    "url": "/spotifycloneappproject/index.html"
+  },
+  {
+    "revision": "d8fab9cc4a3287887bca",
+    "url": "/spotifycloneappproject/static/css/main.63f5bd38.chunk.css"
+  },
+  {
+    "revision": "1b2f559e1e95c115b1cd",
+    "url": "/spotifycloneappproject/static/js/2.dfc953aa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotifycloneappproject/static/js/2.dfc953aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8fab9cc4a3287887bca",
+    "url": "/spotifycloneappproject/static/js/main.7e0f7301.chunk.js"
+  },
+  {
+    "revision": "35d3443709e9f2f71e13",
+    "url": "/spotifycloneappproject/static/js/runtime-main.9b41647c.js"
+  }
+]);
